d1238f13f6d0e5286fc40e1e5e918eb86f847ba5
ThiinhUET <thinh.lv0812@gmail.com>
2022-01-25 20:59:21 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
-
 const app = express();
+const port = process.env.PORT || 3301;
 
-app.use(express.static('./public'));
+app.use(express.static(__dirname + '/views'));
 
-app.listen(3301, () => {
-  console.log('App is running at 3301');
-})
+app.set('view engine', "ejs");
+app.set('views', "./views");
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.get('/contact', (req, res) => {
+    res.render("contact");
+});
+
+app.get('/about', (req, res) => {
+    res.render("about");
+});
+
+app.get('/post', (req, res) => {
+    res.render("post");
+});
+
+
+app.listen(port, () => {
+    console.log(`App is listening at port ${port}`);
+});
